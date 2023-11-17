d194ea17eabe4e1b43c5eef9a181bbfc8d79aa86
puokl <phuoc@sapo.pt>
2023-11-17 13:04:51 +0100

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,6 @@
-// import { useState } from "react";
-// import StartButton from "../components/StartButton";
-// import CodeInput from "../components/CodeInput";
-// import FeedbackDisplay from "../components/FeedbackDisplay";
 import { Link } from "react-router-dom";
 
 function Home() {
-  //   const [gameStarted, setGameStarted] = useState(false);
-  //   const [secretCode, setSecretCode] = useState([]);
-  //   // Additional state like currentGuess, guessHistory, etc.
-
-  //   const startGame = () => {
-  //     // Generate a random code and update the state
-  //     setGameStarted(true);
-  //   };
-
-  //   // Function to handle guesses, update state, etc.
-
   return (
     <div className="app">
       Hello from Home
